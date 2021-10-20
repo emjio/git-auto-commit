0762b0f709482fdf6d98727f2362d39efb2055a3
wangji <wangji@qilingu.com>
2021-10-20 13:08:34 +0800

    opti: 冗余代码

--- src/Scheduler.ts
+++ src/Scheduler.ts
@@ -1,60 +1,57 @@
 let timer: NodeJS.Timeout;
 import * as vscode from 'vscode';
-import {runCommand,throwErrorType } from './utils'
-import {ReminderView} from './TextView'
+import {runCommand,throwErrorType } from './utils';
+import {ReminderView} from './TextView';
 export default class Scheduler {
-    $option: Option
+    $option: Option;
     constructor(prop: Option) {
-        this.$option = { ...prop }
+        this.$option = { ...prop };
     }
     changeOptions(option: Option) {
-        this.$option = option
+        this.$option = option;
     }
     changeListener(e:vscode.TextDocument) {
             if (timer) {
-                clearTimeout(timer)
+                clearTimeout(timer);
             }
             timer = setTimeout(() => {
-                this.run()
-                clearTimeout(timer)
-            }, this.$option.commitTimeInterval)
+                this.run();
+                clearTimeout(timer);
+            }, this.$option.commitTimeInterval);
     }
     private  async _getDiff(path:string){
         let cmd = `cd ${path} && git diff`;
-        return await runCommand(cmd)
+        return await runCommand(cmd);
     }
     private async _getUnCommitChange(path: string) {
         let cmd = `cd ${path}`;
             if (this.$option.autoPush) {
-                cmd = cmd + ' && git push'
+                cmd = cmd + ' && git push';
             } else {
-                cmd = cmd + ' && git status'
+                cmd = cmd + ' && git status';
             }
-            let res = ''
-            return await runCommand(cmd)
+            return await runCommand(cmd);
     }
     async run() {
         if (this.$option.path) {
                 // 执行之前先验证是否有差异
-                const diff = await this._getDiff(this.$option.path)
+                const diff = await this._getDiff(this.$option.path);
                 if(diff!==''){
                     const cmd = `cd ${this.$option.path} && git add . && git commit -n -m "auto-commit" `;
                     try{
-                    const commitRes = await runCommand(cmd)
-                    const UnCommitChange = await this._getUnCommitChange(this.$option.path as string)
+                    const commitRes = await runCommand(cmd);
+                    const unCommitChange = await this._getUnCommitChange(this.$option.path as string);
                     vscode.window.showInformationMessage('代码成功自动commit，是否查看日志', '是', '否').then(result => {
                         if (result === '是') {
-                         ReminderView.show(this.$option.context,{diffRes:diff,commitRes,gitStatus:UnCommitChange},)
-                        } else if (result === '不再提示') {
-                            // 其它操作
-                        }})
+                         ReminderView.show(this.$option.context,{diffRes:diff,commitRes,gitStatus:unCommitChange},);
+                        }});
                     }catch(e:any){
-                        ReminderView.show(this.$option.context,e)
+                        ReminderView.show(this.$option.context,e);
                     }
                 }
             }
         }
     destroy() {
-        clearTimeout(timer)
+        clearTimeout(timer);
     }
 }
